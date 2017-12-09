29f196c79000938951f67ab2d4c07b59673fd8c9
eegee <git.eegee@0wnd.org>
2017-12-09 07:32:52 +0200

    Add solution for 2017 - Day 9

---
+++ 2017/Day 9 - Stream Processing.py
@@ -0,0 +1,24 @@
+stream = open("inputs/day9.txt").readlines()[0]
+score = 0
+depth = 1
+garbage = False
+ignoreNext = False
+garbageRemoved = 0
+for char in stream:
+    if ignoreNext:
+        ignoreNext = False
+    elif char == "!":
+        ignoreNext = True
+    elif char == "{" and not garbage:
+        score += depth * 1
+        depth += 1
+    elif char == "}" and not garbage:
+        depth -= 1
+    elif char == "<" and not garbage:
+        garbage = True
+    elif char == ">":
+        garbage = False
+    elif garbage:
+        garbageRemoved += 1
+print("answer 1:", score)
+print("answer 2:", garbageRemoved)
