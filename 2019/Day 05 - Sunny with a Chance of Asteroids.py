3ae08df028e1d59e4c52b969c03da8f8ee45131e
eegee <github.eegee@0wnd.org>
2019-12-06 13:58:24 +0200

    Add solution for 2019 - Day 5

---
+++ 2019/Day 05 - Sunny with a Chance of Asteroids.py
@@ -0,0 +1,57 @@
+def lookup(int_code, mode, index):
+    if mode == "0":
+        return int_code[int_code[index]]
+    elif mode == "1":
+        return int_code[index]
+    else:
+        exit(str.format("something went wrong (mode:{} index:{})", mode, index))
+
+
+def int_code_compute(int_code, input_value):
+    int_code = int_code.copy()
+
+    i = 0
+    result = 0
+    while True:
+        a, b, c, *de = str(int_code[i]).rjust(5, "0")
+        op_code = "".join(de)
+
+        if op_code == "01":
+            int_code[int_code[i + 3]] = lookup(int_code, c, i + 1) + lookup(int_code, b, i + 2)
+            i += 4
+        elif op_code == "02":
+            int_code[int_code[i + 3]] = lookup(int_code, c, i + 1) * lookup(int_code, b, i + 2)
+            i += 4
+        elif op_code == "03":
+            int_code[int_code[i + 1]] = input_value
+            i += 2
+        elif op_code == "04":
+            result = lookup(int_code, c, i + 1)
+            i += 2
+        elif op_code == "05":
+            if lookup(int_code, c, i + 1) != 0:
+                i = lookup(int_code, b, i + 2)
+            else:
+                i += 3
+        elif op_code == "06":
+            if lookup(int_code, c, i + 1) == 0:
+                i = lookup(int_code, b, i + 2)
+            else:
+                i += 3
+        elif op_code == "07":
+            int_code[int_code[i + 3]] = 1 if lookup(int_code, c, i + 1) < lookup(int_code, b, i + 2) else 0
+            i += 4
+        elif op_code == "08":
+            int_code[int_code[i + 3]] = 1 if lookup(int_code, c, i + 1) == lookup(int_code, b, i + 2) else 0
+            i += 4
+        elif op_code == "99":
+            break
+        else:
+            exit(str.format("something went wrong (pos:{} value:{})", i, op_code))
+
+    return result
+
+
+initial_int_code = list(map(int, open("inputs/day05.txt").read().split(",")))
+print("answer 1:", int_code_compute(initial_int_code, 1))
+print("answer 2:", int_code_compute(initial_int_code, 5))
