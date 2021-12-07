b1137901f5c39887d3029c6f10f35d1238245d05
eegee <github.eegee@0wnd.org>
2021-12-07 22:36:02 +0200

    Add solution for 2021 - Day 07

---
+++ 2021/Day 07 - The Treachery of Whales.py
@@ -0,0 +1,5 @@
+crabs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+crabs = list(map(int, open("inputs/day07.txt").read().split(",")))
+
+print("answer 1:", min(sum(abs(crab - i) for crab in crabs) for i in range(max(crabs))))
+print("answer 2:", min(sum(abs(crab - i) * (abs(crab - i) + 1) // 2 for crab in crabs) for i in range(max(crabs))))
