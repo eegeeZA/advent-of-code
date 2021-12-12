b750bed69aa73b63b38c62ef2c5b85508608c8e5
eegee <github.eegee@0wnd.org>
2021-12-12 19:32:47 +0200

    Add solution for 2021 - Day 12

---
+++ 2021/Day 12 - Passage Pathing.py
@@ -0,0 +1,38 @@
+import collections
+
+caves = [
+    "start-A",
+    "start-b",
+    "A-c",
+    "A-b",
+    "b-d",
+    "A-end",
+    "b-end",
+]
+caves = open("inputs/day12.txt").read().splitlines()
+
+paths = collections.defaultdict(list)
+for cave in caves:
+    start, end = cave.split("-")
+    paths[start].append(end)
+    paths[end].append(start)
+
+
+def end_routes(path_name, extra_visit=False, visited_caves=None):
+    if visited_caves is None:
+        visited_caves = set()
+
+    if path_name == "end":
+        yield True
+
+    visited_caves.add(path_name)
+    for route in paths[path_name]:
+        if route.islower() and route in visited_caves:
+            if extra_visit and route not in ["start", "end"]:
+                yield from end_routes(route, False, visited_caves.copy())
+            continue
+        yield from end_routes(route, extra_visit, visited_caves.copy())
+
+
+print("answer 1:", sum(end_routes("start")))
+print("answer 2:", sum(end_routes("start", True)))
