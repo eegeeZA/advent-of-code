12a2a58ebea3b63f0331c84f5a99a279351d5484
eegee <github.eegee@0wnd.org>
2021-12-18 21:03:06 +0200

    Add solution for 2021 - Day 16

---
+++ 2021/Day 16 - Packet Decoder.py
@@ -0,0 +1,56 @@
+import itertools
+import math
+
+
+def version_totals(binary):
+    version = int(binary[:3], 2)
+    packet_type = int(binary[3:6], 2)
+
+    if packet_type == 4:
+        literal = ""
+        for i in itertools.count():
+            part = binary[6 + i * 5:6 + (i + 1) * 5]
+            literal += part[1:]
+            if part[0] == "0":
+                return version, binary[6 + (i + 1) * 5:], int(literal, 2)
+
+    numbers = []
+    if binary[6] == "0":
+        sub_length = int(binary[7:7 + 15], 2)
+        sub_packet = binary[7 + 15:7 + 15 + sub_length]
+        binary = binary[7 + 15 + sub_length:]
+        while sub_packet:
+            sub_version, sub_packet, number = version_totals(sub_packet)
+            version += sub_version
+            numbers.append(number)
+    elif binary[6] == "1":
+        sub_count = int(binary[7:7 + 11], 2)
+        binary = binary[7 + 11:]
+        for _ in range(sub_count):
+            sub_version, binary, number = version_totals(binary)
+            version += sub_version
+            numbers.append(number)
+
+    result = None
+    if packet_type == 0:
+        result = sum(numbers)
+    elif packet_type == 1:
+        result = math.prod(numbers)
+    elif packet_type == 2:
+        result = min(numbers)
+    elif packet_type == 3:
+        result = max(numbers)
+    elif packet_type == 5:
+        result = int(numbers[0] > numbers[1])
+    elif packet_type == 6:
+        result = int(numbers[0] < numbers[1])
+    elif packet_type == 7:
+        result = int(numbers[0] == numbers[1])
+    return version, binary, result
+
+
+bits = open("inputs/day16.txt").read().strip()
+
+version_total, _, evaluation = version_totals("".join(bin(int(char, 16))[2:].zfill(4) for char in bits))
+print("answer 1:", version_total)
+print("answer 2:", evaluation)
