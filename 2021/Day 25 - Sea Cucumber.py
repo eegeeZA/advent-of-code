a5f23b99ea776095b5204707a6c74d3e38a247c9
eegee <github.eegee@0wnd.org>
2021-12-26 03:37:51 +0200

    Add solution for 2021 - Day 25

---
+++ 2021/Day 25 - Sea Cucumber.py
@@ -0,0 +1,40 @@
+import copy
+
+sea_cucumbers = """
+v...>>.vv>
+.vv>>.vv..
+>>.>v>...v
+>>v>>.>.v.
+v>v.vv.v..
+>.>>..v...
+.vv..>.>v.
+v.v..>>v.v
+....v..v.>
+"""[1:]
+sea_cucumbers = open("inputs/day25.txt").read()
+sea_cucumbers = [[*sea_cucumber] for sea_cucumber in sea_cucumbers.splitlines()]
+
+moving = True
+steps = 0
+while moving:
+    steps += 1
+    moving = False
+
+    sea_cucumbers_snapshot = copy.deepcopy(sea_cucumbers)
+    for i in range(len(sea_cucumbers)):
+        for j in range(len(sea_cucumbers[i])):
+            right = (j + 1) % len(sea_cucumbers[i])
+            if sea_cucumbers_snapshot[i][j] == ">" and sea_cucumbers_snapshot[i][right] == ".":
+                sea_cucumbers[i][j], sea_cucumbers[i][right] = sea_cucumbers[i][right], sea_cucumbers[i][j]
+                moving = True
+
+    sea_cucumbers_snapshot = copy.deepcopy(sea_cucumbers)
+    for i in range(len(sea_cucumbers)):
+        down = (i + 1) % len(sea_cucumbers)
+        for j in range(len(sea_cucumbers[i])):
+            if sea_cucumbers_snapshot[i][j] == "v" and sea_cucumbers_snapshot[down][j] == ".":
+                sea_cucumbers[i][j], sea_cucumbers[down][j] = sea_cucumbers[down][j], sea_cucumbers[i][j]
+                moving = True
+
+print("answer 1:", steps)
+print("answer 2:", "*")
