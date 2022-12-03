32e272549e192c92824e59d69dcba2bd54d27e37
eegee <github.eegee@0wnd.org>
2022-12-03 11:42:40 +0200

    Add solution for 2022 - Day 02

---
+++ 2022/Day 02 - Rock Paper Scissors.py
@@ -0,0 +1,70 @@
+strategy = """A Y
+B X
+C Z""".splitlines()
+# strategy = open("inputs/day02.txt").readlines()
+
+scores = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3,
+}
+
+score = 0
+for turn in strategy:
+    opp, me = turn.split()
+    score += scores[me]
+    if opp == 'A':
+        if me == 'X':
+            score += 3
+        elif me == 'Y':
+            score += 6
+    if opp == 'B':
+        if me == 'Y':
+            score += 3
+        elif me == 'Z':
+            score += 6
+    if opp == 'C':
+        if me == 'Z':
+            score += 3
+        elif me == 'X':
+            score += 6
+print("answer 1:", score)
+
+desired = {
+    'X': {
+        'A': 'Z',
+        'B': 'X',
+        'C': 'Y',
+    },
+    'Y': {
+        'A': 'X',
+        'B': 'Y',
+        'C': 'Z',
+    },
+    'Z': {
+        'A': 'Y',
+        'B': 'Z',
+        'C': 'X',
+    },
+}
+score = 0
+for turn in strategy:
+    opp, me = turn.split()
+    me = desired[me][opp]
+    score += scores[me]
+    if opp == 'A':
+        if me == 'X':
+            score += 3
+        elif me == 'Y':
+            score += 6
+    if opp == 'B':
+        if me == 'Y':
+            score += 3
+        elif me == 'Z':
+            score += 6
+    if opp == 'C':
+        if me == 'Z':
+            score += 3
+        elif me == 'X':
+            score += 6
+print("answer 2:", score)
