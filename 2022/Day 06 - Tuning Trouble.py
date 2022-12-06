0ece116f28eb1d8641bb9299bed0c1ebd17b5a30
eegee <github.eegee@0wnd.org>
2022-12-06 07:15:39 +0200

    Add solution for 2022 - Day 06

---
+++ 2022/Day 06 - Tuning Trouble.py
@@ -0,0 +1,12 @@
+signal = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+signal = open("inputs/day06.txt").read()
+
+for i in range(len(signal)):
+    if len(set(signal[i:i + 4])) == 4:
+        print("answer 1:", i + 4)
+        break
+
+for i in range(len(signal)):
+    if len(set(signal[i:i + 14])) == 14:
+        print("answer 2:", i + 14)
+        break
