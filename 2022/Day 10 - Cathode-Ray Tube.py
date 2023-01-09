fea8b55bef18e10a2e671efc11d335a96a723b59
eegee <github.eegee@0wnd.org>
2023-01-09 19:21:53 +0200

    Add solution for 2022 - Day 10

---
+++ 2022/Day 10 - Cathode-Ray Tube.py
@@ -0,0 +1,172 @@
+instructions = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""[1:].splitlines()
+instructions = open("inputs/day10.txt").read().splitlines()
+
+x = {1: 1}
+cycle = 1
+for instruction in instructions:
+    if "noop" in instruction:
+        cycle += 1
+    else:
+        cycle += 2
+        x[cycle] = int(instruction[5:])
+
+signal = 0
+for i in range(20, 220 + 1, 40):
+    signal += i * sum([value for key, value in x.items() if key <= i])
+print("answer 1:", signal)
+
+print("answer 2:", )
+sprite = 0
+for cycle in range(1, 240 + 1):
+    if cycle in x:
+        sprite += x[cycle]
+    print("🟪" if abs(((cycle - 1) % 40) - sprite) <= 1 else "⬛", end="")
+    if cycle % 40 == 0:
+        print()
