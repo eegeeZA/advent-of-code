98da525b81b1803d99d5c1fda534fff0ab01a3c1
eegee <github.eegee@0wnd.org>
2023-01-22 14:09:07 +0200

    Add solution for 2022 - Day 12

---
+++ 2022/Day 12 - Hill Climbing Algorithm.py
@@ -0,0 +1,68 @@
+import collections
+import math
+
+heightmap = """
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+"""[1:].splitlines()
+heightmap = open("inputs/day12.txt").read().splitlines()
+
+
+def can_reach(current, neighbour):
+    return ord(neighbour) - ord(current) <= 1
+
+
+start = end = None
+routes = collections.defaultdict(list)
+starts = []
+for i, row in enumerate(heightmap):
+    for j, height in enumerate(row):
+        if height == "S":
+            start = (i, j)
+            heightmap[i] = heightmap[i].replace("S", "a")
+        if height == "E":
+            end = (i, j)
+            heightmap[i] = heightmap[i].replace("E", "z")
+        if height == "a":
+            starts.append((i, j))
+for i, row in enumerate(heightmap):
+    for j, height in enumerate(row):
+        if i > 0 and can_reach(height, heightmap[i - 1][j]):
+            routes[i, j].append((i - 1, j))
+        if j > 0 and can_reach(height, heightmap[i][j - 1]):
+            routes[i, j].append((i, j - 1))
+        if i < len(heightmap) - 1 and can_reach(height, heightmap[i + 1][j]):
+            routes[i, j].append((i + 1, j))
+        if j < len(row) - 1 and can_reach(height, heightmap[i][j + 1]):
+            routes[i, j].append((i, j + 1))
+
+steps = {key: math.inf for key in routes.keys()}
+steps[start] = 0
+for route in starts:
+    steps[route] = 0
+
+
+def dijkstra(routes, steps):
+    while routes:
+        next_step = min(routes.keys(), key=lambda x: steps[x])
+        if next_step == end:
+            return steps[next_step]
+        neighbours = routes.pop(next_step)
+
+        for neighbour in neighbours:
+            if neighbour not in routes:
+                continue
+            distance = steps[next_step] + 1
+            if distance < steps[neighbour]:
+                steps[neighbour] = distance
+
+
+steps = {key: math.inf for key in routes.keys()}
+steps[start] = 0
+print("answer 1:", dijkstra(routes.copy(), steps.copy()))
+for route in starts:
+    steps[route] = 0
+print("answer 2:", dijkstra(routes.copy(), steps))
